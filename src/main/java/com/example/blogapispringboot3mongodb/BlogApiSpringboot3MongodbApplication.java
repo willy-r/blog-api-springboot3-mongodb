b535598dcb6fd0d716396e89f25ea411839a2ccb
William Rodrigues <williamrodrigues2442@gmail.com>
2023-04-19 17:14:48 -0300

    feat: initial commit

---
+++ src/main/java/com/example/blogapispringboot3mongodb/BlogApiSpringboot3MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogapispringboot3mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiSpringboot3MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiSpringboot3MongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogapispringboot3mongodb/BlogApiSpringboot3MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogapispringboot3mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiSpringboot3MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
