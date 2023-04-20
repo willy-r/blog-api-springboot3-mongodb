d16bb82ca70e3b5fbabb6d56c7b819e4dca2e966
William Rodrigues <williamrodrigues2442@gmail.com>
2023-04-20 13:44:24 -0300

    feat: add initial users on db

---
+++ src/main/java/com/example/blogapispringboot3mongodb/config/Instantiation.java
@@ -0,0 +1,24 @@
+package com.example.blogapispringboot3mongodb.config;
+
+import com.example.blogapispringboot3mongodb.domain.User;
+import com.example.blogapispringboot3mongodb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+        User user1 = new User(null, "Maria Brown", "maria@gmail.com");
+        User user2 = new User(null, "Alex Green", "alex@gmail.com");
+        User user3 = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(user1, user2, user3));
+    }
+}
