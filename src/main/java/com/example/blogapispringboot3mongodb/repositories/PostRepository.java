45520067b70ce7704fa11ce4e6f57a5e0728b41e
William Rodrigues <williamrodrigues2442@gmail.com>
2023-05-01 19:55:12 -0300

    feat: using mongo query to create a simple consult

--- src/main/java/com/example/blogapispringboot3mongodb/repositories/PostRepository.java
+++ src/main/java/com/example/blogapispringboot3mongodb/repositories/PostRepository.java
@@ -2,11 +2,15 @@ package com.example.blogapispringboot3mongodb.repositories;
 
 import com.example.blogapispringboot3mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> customFindByTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
